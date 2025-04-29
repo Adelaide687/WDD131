ce308b789aeacb5a1ccbd3578002025dcb41ea05
Adelaide687 <pla24004@byui.edu>
2025-04-29 08:57:58 -0600

    first

---
+++ WhateverYouLike.js
@@ -0,0 +1,7 @@
+const myApples = 4;
+const friendApples = 2;
+let totalApples = myApples + friendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalAppleElement").textContent = totalApples;
