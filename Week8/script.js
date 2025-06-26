2b58685af4b6f15d2b2a38f614305b311b70ded6
Adelaide687 <pla24004@byui.edu>
2025-06-25 18:34:04 -0600

    git

--- Week8/script.js
+++ Week8/script.js
@@ -3,7 +3,7 @@ const character = {
     class: "Swamp Beat Diplomat",
     level: 5,
     health: 100,
-    image: 'https://andejuli.github.io/img/snortleblat.png',
+    image: "https://andejuli.github.io/img/snortleblat.png",
     attacked() {
       if (this.health >= 20) {
         this.level -= 1;
